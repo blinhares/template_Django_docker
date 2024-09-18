56068a6a7cb12fa291dce03b79cfe8eccbc53c57
LinharesB <b_linhares@outlook.com.com>
2024-09-18 19:34:05 -0300

    +docs

--- djangoapp/app_exemplo/forms.py
+++ djangoapp/app_exemplo/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from . import models
 
 class DateInput(forms.DateInput):
-    """Cria um Widget para colocar datas."""
+    """Cria um Widget para colocar datas.
+    ver: https://docs.djangoproject.com/en/5.1/ref/forms/widgets/ """
 
     input_type = 'date'
 class ExempleForm(forms.ModelForm):
